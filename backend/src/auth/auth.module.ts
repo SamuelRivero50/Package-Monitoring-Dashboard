5e3a5fd9b1ab52c84041b49259b4c18292d124e8
jayounghoyos <jayoungh@eafit.edu.co>
2026-04-21 12:02:52 -0500

    feat: add auth module structure

---
+++ backend/src/auth/auth.module.ts
@@ -0,0 +1,23 @@
+// External imports
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+// Internal imports
+import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
+import { jwtConstants } from './constants';
+import { UsersModule } from '../users/users.module';
+
+@Module({
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  controllers: [AuthController],
+  providers: [AuthService],
+})
+export class AuthModule {}
